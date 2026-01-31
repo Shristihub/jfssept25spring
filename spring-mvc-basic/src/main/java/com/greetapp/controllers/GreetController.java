97e609c7621b668e1553aaae34473a71f9202297
shristitechacademy <sripriyamathan@gmail.com>
2026-01-31 12:45:23 +0530

    mvc

---
+++ spring-mvc-basic/src/main/java/com/greetapp/controllers/GreetController.java
@@ -0,0 +1,54 @@
+package com.greetapp.controllers;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class GreetController {
+
+	// return value should be a string - name of the view page
+	@GetMapping("/greet")
+	public String greetMsg(Model model) {
+		// call the service layer and get the data
+		String message = "Have a great day";
+		// bundle this data - attach it to a model
+		model.addAttribute("mymessage",message);
+		// return the view page name
+		return "success"; 
+	}
+	
+	
+	// return value should be a string - name of the view page
+	@GetMapping("/show-books")
+	public String showBooks(ModelMap model) {
+		// call the service layer and get the data
+		// bundle this data - attach it to a model
+		model.addAttribute("books",Arrays.asList("Java","Spring","Microservices"));
+		// return the view page name
+		return "success"; 
+	}
+
+	// return value is ModelAndView object - pasiing model and view together
+		@GetMapping("/welcome")
+		public ModelAndView welcome() {
+			// call the service layer and get the data
+			String data = "Welcome to MVC";
+			// bundle this data - attach it to a modeland view object
+			ModelAndView modelAndView = new ModelAndView("success", "mymessage",data);
+			// return the model and view object
+			return modelAndView; 
+		}
+}
+
+
+
+
+
+
+
+
---
+++ spring-mvc-basic/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<a href="greet">Greet Message</a><br>
+<a href="show-books">Show Books</a><br>
+<a href="welcome">Welcome Message </a><br>
+</body>
+</html>
